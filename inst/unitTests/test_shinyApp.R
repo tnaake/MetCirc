936a07df0e6ef3347f0aff76992ffe868ae5de5e
Thomas Naake <tn299@cam.ac.uk>
2017-04-03 15:16:27 +0000

    update unit tests for shinyApp, plottingFunctions and convert2MSP
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MetCirc@127995 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_shinyApp.R
+++ inst/unitTests/test_shinyApp.R
@@ -28,6 +28,16 @@ test_printInformationSelect <- function() {
                     linkMatrixThreshold = linkMat_thr, 
                     similarityMatrix = orderedSimMat),
         "ANT_1052.94/1018.15 connects to <br/> ANT_1063.44/1017.07 <br/>ANT_1398.71/1015.75 <br/>LIM_1398.71/1015.75")
+    checkEquals(MetCirc:::printInformationSelect( 
+        groupname = rownames(similarityMat), msp = finalMSP, ind = ind, 
+        lMatInd = linkMatInds, linkMatrixThreshold = linkMat_thr, 
+        similarityMatrix = similarityMat),
+        "SPL_966.94/990.15 (Unknown, Unknown, Unknown, Unknown) connects to  <br/>ANT_1063.44/1017.07 (0.01, Unknown, Unknown, Unknown, Unknown)<br/> ANT_1398.71/1015.75 (0.002, Unknown, Unknown, Unknown, Unknown)<br/> LIM_1398.71/1015.75 (0.002, Unknown, Unknown, Unknown, Unknown)<br/> ANT_1052.94/1018.15 (0.043, Unknown, Unknown, Unknown, Unknown)<br/>")
+    checkEquals(MetCirc:::printInformationSelect( 
+        groupname = rownames(similarityMat), msp = finalMSP, ind = ind, 
+        lMatInd = NULL, linkMatrixThreshold = linkMat_thr, 
+        similarityMatrix = similarityMat),
+        "SPL_966.94/990.15 (Unknown, Unknown, Unknown,Unknown) does not connect to any feature")
 }
 ## END unit test printInformationSelect
 
@@ -35,6 +45,8 @@ test_printInformationSelect <- function() {
 test_createOrderedSimMat <- function() {
     checkException(createOrderedSimMat(similarityMat, order = "foo"))
     checkException(createOrderedSimMat(order = "mz"))
+    checkException(createOrderedSimMat(order = "retentionTime"))
+    checkException(createOrderedSimMat(order = "clustering"))
     checkEquals(dim(simMat), dim(similarityMat))
     checkEquals(colnames(simMat), rownames(simMat))
     checkTrue(is.matrix(simMat))
