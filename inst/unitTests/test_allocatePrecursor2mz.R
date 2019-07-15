2bdf460e888319ca49a051acb2686af0ec2a7a35
tnaake <thomasnaake@gmx.de>
2019-07-15 15:20:16 +0200

    remove allocatePrecursor2mz and unittests

--- inst/unitTests/test_allocatePrecursor2mz.R
+++
@@ -1,8 +0,0 @@
-## START unit test allocatePrecursor2mz
-testAllocatePrecursor2MZ <- allocatePrecursor2mz(sd01_outputXCMS, sd02_deconvoluted)
-
-test_allocatePrecursor2mz <- function() {
-    checkTrue(is.data.frame(testAllocatePrecursor2MZ))
-    checkEquals(dim(testAllocatePrecursor2MZ), c(360, 202))
-}
-## END unit test allocatePrecursor2mz
